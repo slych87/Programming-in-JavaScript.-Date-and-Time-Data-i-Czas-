28e300e8a6357fe071a86a053678dc55f4be9714
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-16 10:22:21 +0100

    done

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 const clock = () => {
- const time = new Date();
- // console.log(time.toLocaleString());
- // console.log(time.toLocaleDateString());
- const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
- const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
- const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+    const time = new Date()
+    // console.log(time.toLocaleString());
+    // console.log(time.toLocaleDateString());
+    const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
 
- document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
+    document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(clock, 1000);
+setInterval(clock, 1000); 
